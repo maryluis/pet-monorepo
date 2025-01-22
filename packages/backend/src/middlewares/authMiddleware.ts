2c59cd07daf5c4fc32a6847ea4326f7dd81fad47
Mariia Hlamazda <mariia.hlamazda@novaience.com>
2025-01-22 14:25:31 +0200

    [68 errors]

--- packages/backend/src/middlewares/authMiddleware.ts
+++ packages/backend/src/middlewares/authMiddleware.ts
@@ -2,17 +2,21 @@ import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 import { errorCodes } from '@shared/constants';
 
-export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+interface RequestWithId extends Request {
+  id: string,
+}
+
+export const authMiddleware = (req: RequestWithId, res: Response, next: NextFunction) => {
   const token = req.header('Authorization')?.replace('Bearer ', '');
   if (!token) {
-    return res.status(errorCodes.accessDenied).json({ message: 'Access denied' });
+    res.status(errorCodes.accessDenied).json({ message: 'Access denied' });
   }
 
   const SECRET_KEY = process.env.SECRET_KEY;
 
   jwt.verify(token, SECRET_KEY, (err, decoded) => {
     if (err) {
-      return res.status(errorCodes.invalidToken).json({ message: 'Invalid token', code: errorCodes.invalidToken });
+      res.status(errorCodes.invalidToken).json({ message: 'Invalid token', code: errorCodes.invalidToken });
     }
     else {
       req.id = decoded.id;
