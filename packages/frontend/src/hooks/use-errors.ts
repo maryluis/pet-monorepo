5ba6d0fcd18c18f3be35fab897e2a09ef2551a53
Mariia Hlamazda <mariia.hlamazda@novaience.com>
2025-01-28 15:23:28 +0200

    [test deploy backend on correct branch without command word]

--- packages/frontend/src/hooks/use-errors.ts
+++ packages/frontend/src/hooks/use-errors.ts
@@ -26,7 +26,7 @@ export const useErrors = () => {
       navigate(Paths.loginWithFallback(location.pathname));
       return e;
     }
-    console.error(`error ${e}`);
+    console.error(`err ${e}`);
   };
 };
 
